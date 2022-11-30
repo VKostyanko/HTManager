0b0fe0c49a5caac72b937722d84bd31ebfb76f4e
VKostyanko <90458703+VKostyanko@users.noreply.github.com>
2022-11-30 13:48:22 +0200

    init

---
+++ src/main/kotlin/com/ktrack/htmanager/HtManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.ktrack.htmanager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HtManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<HtManagerApplication>(*args)
+}
---
+++ src/test/kotlin/com/ktrack/htmanager/HtManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ktrack.htmanager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HtManagerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
